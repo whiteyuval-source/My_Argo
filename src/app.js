15351fd39acf68760ae4b194210162714ea1dce7
whiteyuval-source <white.yuval@gmail.com>
2025-12-02 12:16:43 +0200

    Update app.js

--- src/app.js
+++ src/app.js
@@ -7,7 +7,7 @@ const port = 3000;
 const server = http.createServer((req, res) => {
   res.statusCode = 200;
   res.setHeader('Content-Type', 'text/plain');
-  res.end('Hello, World 4!\n');
+  res.end('Hello, World 44!\n');
 });
 
 server.listen(port, hostname, () => {
